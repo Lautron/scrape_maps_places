04aaf0f87dbd8285225fdd859504157971794d0c
Lautaro Bachmann <laurigbach@gmail.com>
2020-12-02 20:00:05 -0300

    Emptied url list

--- scrape_attractions.py
+++ scrape_attractions.py
@@ -88,11 +88,7 @@ def scrape_attractions(urls):
 
 
 if __name__ == "__main__":
-    urls = [
-        'https://www.google.com/search?tbm=lcl&sxsrf=ALeKk00mjEPKdLuHIF69pewXC80KmlfwUA%3A1606921055223&ei=X6vHX971DIrA5OUP7rikqAk&hotel_occupancy=&q=alojamiento+puerto+rico+misiones&oq=alojamiento+puer&gs_l=psy-ab.3.0.35i39k1j0i67k1l2j0l7.484399.486313.0.487273.16.15.0.0.0.0.220.1986.0j12j2.14.0....0...1c.1.64.psy-ab..2.14.1980...46j0i433k1j46i433i131k1j46i433k1j46i67k1j0i433i131i67k1j0i20i263k1.0.T69IelvrPlY#rlfi=hd:;si:11749626320974400381,y,82SO0VBCrQs;mv:[[-26.809706799999997,-54.998407],[-26.8219182,-55.027229]]',
-        'https://www.google.com/search?tbs=lf:1,lf_ui:9&tbm=lcl&sxsrf=ALeKk02d_99VcQiICEgnljlwgVEcpiloDg:1606930075482&q=restaurantes+puerto+rico+misiones&rflfq=1&num=10&sa=X&ved=2ahUKEwj9ts-C6a_tAhUSGLkGHZN2Cs8QjGp6BAgFEEs&biw=1366&bih=601#rlfi=hd:;si:;mv:[[-26.799132,-55.020778299999996],[-26.8239037,-55.032233399999996]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:9',
-        'https://www.google.com/search?biw=1366&bih=601&tbm=lcl&sxsrf=ALeKk03JVHC_f42WWM_5q1FUyPivFUkGdw%3A1606930082130&ei=os7HX529B_nB5OUP7JGgiAY&q=atracciones+puerto+rico+misiones&oq=atracciones+puerto+rico+misiones&gs_l=psy-ab.3..35i39k1.69904.71136.0.71611.11.9.0.0.0.0.298.1070.0j4j2.6.0....0...1c.1.64.psy-ab..8.1.297....0.NQF2feaXwos#rlfi=hd:;si:;mv:[[-26.7487985,-54.9150596],[-26.9348076,-55.0701689]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:1',
-        ]
+    urls = []
     for url in urls:
         scrape_attractions(url)
 
